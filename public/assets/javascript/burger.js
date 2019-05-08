742f74e89573339fdfd0e7369c4cc04c08f00769
solasalami <salamisola@gmail.com>
2019-05-08 18:55:55 -0400

    changes made to clear the issue with this file containing the ajax that call the submit and onclick events used to trigger the actions

--- public/assets/javascript/burger.js
+++ public/assets/javascript/burger.js
@@ -1,5 +1,64 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#newburger")
+        .val()
+        .trim(),
+      devoured: 0
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function () {
+      console.log("Added new burger");
+      location.reload();
+    });
+  });
+
+  $(".eatburger").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function () {
+      console.log("Burger devoured");
+      location.reload();
+    });
+  });
+
+  $(".trashburger").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax({
+      type: "DELETE",
+      url: "/api/burgers/" + id
+    }).then(location.reload());
+  });
+});
+
+
+
+
+
+
+//====================================================================================
+
+
+
+/*$(function () {
   $(".devoured").on("click", function (event) {
     var id = $(this).data("id");
     var burgerDevoured = $(this).data("true");
@@ -42,4 +101,4 @@ $(function () {
       }
     );
   });
-});
+});*/
