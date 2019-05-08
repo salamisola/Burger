3c8cc8a4a04476e5efceec1fbd713c556ba2466d
solasalami <salamisola@gmail.com>
2019-05-08 18:42:45 -0400

    the burger.js updated to conform with instructions of the assignment

--- models/burger.js
+++ models/burger.js
@@ -8,22 +8,23 @@ var burger = {
     });
   },
   // The variables cols and vals are arrays.
-  create: function (cols, vals, cb) {
-    orm.create("burgers", cols, vals, function (res) {
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
-  update: function (objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function (res) {
-      cb(res);
-    });
-  }
-delete: function (condition, cb) {
-    orm.delete("burgers", condition, function (res) {
+  updateOne: function (objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
       cb(res);
     });
   }
 };
+/* deleteOne: function (condition, cb) {
+    orm.deleteOne("burgers", condition, function (res) {
+      cb(res);
+    });
+  }
+}; */
 // Export the database functions for the controller (burgersController.js).
 module.exports = burger;
 
