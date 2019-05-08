5fade78ca1a157e40e1ffbc1a87e39658d21ac96
solasalami <salamisola@gmail.com>
2019-05-08 17:22:28 -0400

    controller file updated

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,68 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var burger = require('../models/burger.js');
 
-router.get('/', function (req, res) {
-    res.redirect('/burgers');
-});
-
-router.get('/burgers', function (req, res) {
-    burger.all(function (burger_data) {
-        res.render('index', { burger_data });
-    });
-});
-
-router.post('/burgers/create', function (req, res) {
-    burger.create(req.body.burger_name, function (result) {
-        console.log(result);
-        res.redirect('/');
-    });
-});
-
-router.put('/burgers/update', function (req, res) {
-    burger.update(req.body.burger_id, function (result) {
-        console.log(result);
-        res.redirect('/');
-    });
-});
-
-module.exports = router;
-
-/* var express = require("express");
+var express = require("express");
 
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burgers.js");
 
-router.get("/", function (req, res) {
-    res.redirect("/burgers");
-});
-
-router.get("/burgers", function (req, res) {
-    burger.all(function (burger_data) {
-        res.render("index", { burger_data });
-    });
-});
-
-router.post("/burgers/create", function (req, res) {
-    burger.create(req.body.burger_name, function (result) {
-        console.log(result);
-        res.redirect("/");
-    });
-});
-
-router.put("/burgers/update", function (req, res) {
-    burger.update(req.body.burger_id, function (result) {
-        console.log(result);
-        res.redirect("/");
-    });
-});
-
-module.exports = router;
- */
 // Create all our routes and set up logic within those routes where required.
-/*router.get("/", function (req, res) {
+router.get("/", function (req, res) {
     burger.all(function (data) {
         var hbsObject = {
             burgers: data
@@ -78,7 +23,7 @@ router.post("/api/burgers", function (req, res) {
     ], [
             req.body.burger_name, req.body.devoured
         ], function (result) {
-            // Send back the ID of the new quote
+            // Send back the ID of the new burger
             res.json({ id: result.insertId });
         });
 });
@@ -100,6 +45,18 @@ router.put("/api/burgers/:id", function (req, res) {
     });
 });
 
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.delete(condition, function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 // Export routes for server.js to use.
 module.exports = router;
 
@@ -133,3 +90,62 @@ router.put('/burgers/update', function(req, res) {
 });
 
 module.exports = router;*/
+
+
+
+/*router.get("/", function (req, res) {
+    res.redirect("/burgers");
+});
+
+router.get("/burgers", function (req, res) {
+    burger.all(function (burger_data) {
+        res.render("index", { burger_data });
+    });
+});
+
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+router.put("/burgers/update", function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
+        console.log(result);
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
+
+/* var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/', function (req, res) {
+    res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+    burger.all(function (burger_data) {
+        res.render('index', { burger_data });
+    });
+});
+
+router.post('/burgers/create', function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect('/');
+    });
+});
+
+router.put('/burgers/update', function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
+        console.log(result);
+        res.redirect('/');
+    });
+});
+
+module.exports = router; */
+//
